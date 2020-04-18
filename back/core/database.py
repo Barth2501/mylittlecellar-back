f71195f14387cea58eb2ecf8347aa1bba6e81bf7
barth2501 <lancelot2501>
2020-04-18 11:31:26 +0200

    add database

--- back/core/database.py
+++ back/core/database.py
@@ -3,5 +3,5 @@ from peewee import PostgresqlDatabase
 from .config import config
 from urllib.parse import urlparse
 
-url = urlparse('postgres://sajlrqdaakhmxf:c459334dcbab2bdfcf56b6b5e49b581bd4483e7aeca6604480280b19e95ed57d@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d3757og6vdih1')
+url = urlparse('postgres://mjxjszztlfhttf:cfe81c3a20b8a422838a2e944eaa7d64012df025ce3ae04d1e565ccfd87d5763@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d8k6to5ro8o4e7')
 db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
